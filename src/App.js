3b03b16112675bd515e3bc9bdd2de28229e56627
harshika <harshikaasiwal711@gmail.com>
2025-07-15 02:58:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Product from "./Pages/Product";
 import Footer from "./Components/Footer/Footer";
+import men_bannar from "./Components/Assets/Assets/Frontend_Assets/banner_mens.png";
+import women_bannar from "./Components/Assets/Assets/Frontend_Assets/banner_women.png";
+import kid_bannar from "./Components/Assets/Assets/Frontend_Assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,9 +18,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category="men" />} />
-          <Route path="/women" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/men"
+            element={<ShopCategory category="men" banner={men_bannar} />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory category="women" banner={women_bannar} />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory category="kid" banner={kid_bannar} />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
